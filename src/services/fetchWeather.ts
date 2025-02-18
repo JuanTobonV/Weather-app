d54786b8a2e4d5c99b0cd5563a9f0e5861ce0aa1
Juan Tobon Villa <jujotovil@gmail.com>
2025-02-18 16:16:37 -0500

    implementing axios

--- src/services/fetchWeather.ts
+++ src/services/fetchWeather.ts
@@ -1,28 +1,20 @@
-import { currentConditions } from "../types/weatherType";
+import axios from "axios";
+import { currentConditions } from "../types/WeatherType";
 
-export async function fetchWeather(): Promise<currentConditions> {
+export async function fetchWeather(){
 
     const URL = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Medellin/today?unitGroup=metric&key=GXQ7C6R6WRMZY2XTDDFMUL9UE&contentType=json'
 
     try {
-        const response = await fetch(URL);
-        if(!response.ok){
-            throw new Error(`Response status: ${response.status}`)        
+        const response = await axios.get(URL);
+        console.log(response.data);
+    }
+    catch(error){
+        if (axios.isAxiosError(error) && error.response) {
+            console.log(error.response);
         }
-
-        const json = await response.json();
-        console.log(json);
-        return json as currentConditions;
-
-    } catch (error) {
-        console.log(error, "paila");
-        throw error;
     }
 }
 
-export const apiResponse = fetchWeather().then((data) => {
-    console.log(data.humidity);
-}).catch((error) => {
-    console.error(error);
-});
+export const apiResponse = fetchWeather()
 
