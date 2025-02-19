1a1d5b14abd9d09a8df25d65f63b6e1518e7d1ee
Juan Tobon Villa <jujotovil@gmail.com>
2025-02-19 11:33:21 -0500

    fetching data

--- src/types/WeatherType.ts
+++ src/types/WeatherType.ts
@@ -1,6 +1,14 @@
-export interface currentConditions {
+type forecastTypes = {
+    dateTime: string
+    temp: number
+}
+
+export type currentConditions = {
     humidity: number ,
     precip: number , 
     windspeed: number,
     temp:number,
+    conditions: string,
+    cityName: string,
+    forecast: forecastTypes[];
 }
